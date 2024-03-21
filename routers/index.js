a82d6e91d065cef22fca1062cffcdea443d1ccbd
SaintMichael21 <saaintmichael@gmail.com>
2024-03-21 14:03:31 +0700

    second: books

--- routers/index.js
+++ routers/index.js
@@ -8,11 +8,11 @@ const ControllerLogout = require("../controllers/controllerLogout.js");
 const ControllerBooks = require("../controllers/controllerBooks.js");
 const ControllerAdmin = require("../controllers/controllerAdmin.js");
 
-// // get register
-// router.get("/register", ControllerRegister.registerForm);
+// get register
+router.get("/register", ControllerRegister.registerForm);
 
-// // post register
-// router.post("/register", ControllerRegister.postRegister);
+// post register
+router.post("/register", ControllerRegister.postRegister);
 
 // get login
 router.get("/login", ControllerLogin.loginForm);
@@ -44,52 +44,52 @@ router.use(function (req, res, next) {
   }
 });
 
-// /*
-// per satu routing
-// const coba = function (req, res, next) {
-//   console.log("Time : ", Date.now());
-//   next();
-// };
-// router.get("/authors",coba, Controller.readAuthors);
-// */
+/*
+per satu routing
+const coba = function (req, res, next) {
+  console.log("Time : ", Date.now());
+  next();
+};
+router.get("/authors",coba, Controller.readAuthors);
+*/
 
-// //get seeDetail - halaman detail buku - bisa beli
-// router.get("/books/:id", ControllerBooks.renderDetailBook);
+//get seeDetail - halaman detail buku - bisa beli
+router.get("/books/:id", ControllerBooks.renderDetailBook);
 
-// //get transaction - halaman transaksi user - read doang - id user
-// router.get("/transaction/:id", ControllerBooks.renderTransactions);
+//get transaction - halaman transaksi user - read doang - id user
+router.get("/transaction/:id", ControllerBooks.renderTransactions);
 
-// //middleware - global - admin
-// router.use(function (req, res, next) {
-//   // console.log(req.session);
-//   if (req.session.role !== "admin") {
-//     const error = `Kusus Admin saja`;
-//     res.redirect(`/login?error=${error}`);
-//   } else {
-//     next();
-//   }
-// });
+//middleware - global - admin
+router.use(function (req, res, next) {
+  // console.log(req.session);
+  if (req.session.role !== "admin") {
+    const error = `Kusus Admin saja`;
+    res.redirect(`/login?error=${error}`);
+  } else {
+    next();
+  }
+});
 
-// //get admin - halaman admin bisa nambahin, delete, update
-// router.get("/admin", ControllerAdmin.renderBooksAdmin);
+//get admin - halaman admin bisa nambahin, delete, update
+router.get("/admin", ControllerAdmin.renderBooksAdmin);
 
-// //nambahin
-// router.get("/admin/books", ControllerAdmin.renderAddBooksAdmin);
-// router.post(
-//   "/admin/books/:id",
-//   images.single("imageUrl"),
-//   ControllerAdmin.handleAddBooksAdmin
-// );
+//nambahin
+router.get("/admin/books", ControllerAdmin.renderAddBooksAdmin);
+router.post(
+  "/admin/books/:id",
+  images.single("imageUrl"),
+  ControllerAdmin.handleAddBooksAdmin
+);
 
-// //delete
-// router.get("/admin/books/delete/:id", ControllerAdmin.renderDeleteBooksAdmin);
+//delete
+router.get("/admin/books/delete/:id", ControllerAdmin.renderDeleteBooksAdmin);
 
-// //update
-// router.get("/admin/books/edit", ControllerAdmin.renderEditBooksAdmin);
-// router.post(
-//   "/admin/books/edit/:id",
-//   images.single("imageUrl"),
-//   ControllerAdmin.handleEditBooksAdmin
-// );
+//update
+router.get("/admin/books/edit", ControllerAdmin.renderEditBooksAdmin);
+router.post(
+  "/admin/books/edit/:id",
+  images.single("imageUrl"),
+  ControllerAdmin.handleEditBooksAdmin
+);
 
 module.exports = router;
