8477223eb1bdc4fa797b43f11fca7bfe356adb3c
SaintMichael21 <saaintmichael@gmail.com>
2024-03-21 13:01:56 +0700

    Michael:Fitur Login dan Logout

--- controllers/controllerLogin.js
+++ controllers/controllerLogin.js
@@ -3,6 +3,43 @@ const formatRupiah = require("../helpers/formatRupiah.js");
 const bcrypt = require("bcryptjs");
 const { Op } = require("sequelize");
 
-class ControllerLogin {}
+class ControllerLogin {
+  static async loginForm(req, res) {
+    try {
+      const { error } = req.query;
+      res.render("loginForm", { error });
+    } catch (error) {
+      console.log(error);
+      res.send(error);
+    }
+  }
+
+  static async postLogin(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      let user = await User.findOne({ where: { email } });
+
+      if (user) {
+        const isValidPassword = bcrypt.compareSync(password, user.password);
+
+        if (isValidPassword) {
+          req.session.userId = user.id; //set session di controller login
+          req.session.role = user.role;
+          return res.redirect("/");
+        } else {
+          const error = `Invalid Password or Email`;
+          return res.redirect(`/login?error=${error}`);
+        }
+      } else {
+        const error = `Invalid Password or Email`;
+        return res.redirect(`/login?error=${error}`);
+      }
+    } catch (error) {
+      console.log(error);
+      res.send(error);
+    }
+  }
+}
 
 module.exports = ControllerLogin;
